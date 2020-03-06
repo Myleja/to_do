bb636c5a53866a22f7d999dc1aa6788bc351895f
Myleja <56394362+Myleja@users.noreply.github.com>
2020-03-06 18:16:21 +0000

    commit1

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-window.onload = function (){
+window.onload = function () {
 
-}
+
 //variables
 let form = document.getElementById("form");
 let input = document.getElementById("input");
@@ -18,72 +18,75 @@ btn.addEventListener("click", boxChecked);
 //event listener for clear list
 btnClr.addEventListener("click", clearlist);
 
+//input.addEventListener("keydown", addTodoItem);
+
 if (localStorage.length <= 0) {
 btnClr.style.display = "none"; //hide clear btn
-console.log("button");
+console.log("button")
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //add todo item to list
 function addTodoItem() {
     if(input.value === "") {
         alert("You must enter some value!");
     } else {
-        if (list.style.borderTop === "") {
-            console.log("here!");
+        if(list.style.borderTop === "") {
+            console.log("here!")
             list.style.borderTop = "2px solid white";
             btnClr.style.display = "inline";
         }
+        let text = input.value;
+        let item = `<li id="li-${id}">${text}<input id="box-${id}" class="checkboxes type="checkbox"></li>`;
+        list.insertAdjacentHTML('beforeend', item);
+        liItem = {item: text, checked: false};
+        todoList.push(liItem);
+        id++;
+        addToLocalStorage();
+        form.reset();
+    } 
+}
+//adding string through style to list item
+function boxChecked(event) {
+    const element = event.target;
+    if(element.type === "checkbox") {
+        element.parentNode.style.textDecoration = "line-through";
+        todoList = JSON.parse(localStorage.getItem("todoList"));
+        todoList[element.id.spit('-')[1] - 1].checked = element.checked.toString();
+        localStorage.setItem("todoList", JSON.stringify(todoList));
     }
 }
-let text = input.value;
-let item = `<li id="li-${id}">{text}<input id="box-${id}" class="checkboxes" type="checkbox"></li>`;
-list.insertAdjacentHTML('beforeend', item);
-liItem = {
-    item: text,
-    checked:false
-};
-todoList.push(liItem);
-id++;
-addToLocalStorage();
-form.reset();
-
-
-
-
-
-
-
+//adding data tp local storage
+function addToLocalStorage() {
+    if (typeof (Storage) !== "undefined") {
+        localStorage.setItem("todoList", JSON.stringify(todoList));
+    } else {
+        alert("browser doesnt support local storage!");
+    }
+}
+//display all todo list
+function displayList() {
+    list.style.borderTop = "2px solid white";
+    todoList = JSON.parse(localStorage.getItem("todoList"));
+    todoList.forEach(function (element) {
+        console.log(element.item);
+        let text = element.item;
+        let item = `<li id="li-${id}">${text}<input id="box-${id}" class="checkboxes" type="checkbox"></li>`;
+        list.insertAdjacentHTML("beforebegin", item);
+        //if there is a checked box, then style
+        if(element.checked) {
+            let li = document.getElementById("li-" + id);
+            li.style.textDecoration = "line-through";
+            li.childNodes[1].checked = element.checked;
+        }
+        id++;
+    
+    }
+}
+//clear list event listener
+function clearlist() {
+    todoList = [];
+    localStorage.clear();
+    list.innerHTML = "";
+    btnClr.style.display = "none";
+    list.style.borderTop = "";
+}
+}
